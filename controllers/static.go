64abeab9632f2dd9697f3065a3a66a753e7ad182
LoBo <bolatlobakbai@gmail.com>
2024-07-08 01:00:04 +0500

    Made FAQ page use given dynamic data

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/boaltl/lenslocked/views"
@@ -9,5 +10,28 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes, we offer trial for 7 days.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "From 9 am to 6 pm.",
+		},
+		{
+			Question: "How can we get in touch with you?",
+			Answer:   `You can email us at <a href="mailto:bolatlobakbai@gmail.com">bolatlobakbai@gmail.com</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }

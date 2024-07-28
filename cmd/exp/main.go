8a40c2ca820e5d9f4692de59c67484e22970fc98
LoBo <bolatlobakbai@gmail.com>
2024-07-28 20:32:38 +0500

    Exp/main has some work with postgres.

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,31 +1,51 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type User struct {
-	Name string
-	Age  int
-	Bio  string
-}
-
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	// here first argument "pgx" is just driver name to register, next argument provides info about dataSourceName for connection
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-	user := User{
-		Name: "Bolat Nazar",
-		Age:  20,
-		Bio:  `<script>alert("Haha, you have been hx304-3!");</script>`,
-	}
-
-	err = t.Execute(os.Stdout, user)
-
+	defer db.Close()
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connected")
 
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users(
+			id SERIAL PRIMARY KEY,
+			name TEXT,
+			email TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS orders(
+			id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL,
+			amount INT,
+			description TEXT
+		);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Tables are created")
+	name, email := "New", "New@gmail.com"
+	row := db.QueryRow(`
+		INSERT INTO users (name, email) 
+		VALUES($1, $2) RETURNING id;`, name, email)
+	var id int
+	err = row.Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("User created. id =", id)
 }
